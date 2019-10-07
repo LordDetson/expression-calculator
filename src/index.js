eccf227585c8b9d7f408f7602433e0ffbf29e1a5
Lord_Detson <babanin.dima@gmail.com>
2019-10-07 19:36:25 +0300

    feat: implemented expressionCalculator function

--- src/index.js
+++ src/index.js
@@ -1,10 +1,52 @@
+let Calc = require('./exprcalc');
+
 function eval() {
     // Do not use eval!!!
     return;
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    if (!check(expr, [['(', ')']])) {
+        throw "ExpressionError: Brackets must be paired";
+    }
+    return (new Calc()).compile(expr).calc();
+}
+
+function check(str, bracketsConfig) {
+    let arr = [];
+    let buff = null;
+    let isCorrect = false;
+    let isBracket = false;
+    for (let i = 0; i < str.length; i++) {
+        if (arr.length === 0) {
+            for (let j = 0; j < bracketsConfig.length; j++) {
+                if (str[i] === bracketsConfig[j][0] || str[i] === bracketsConfig[j][1]) {
+                    arr.push(str[i]);
+                    break;
+                }
+            }
+        } else {
+            buff = arr.pop();
+            isCorrect = false;
+            isBracket = false;
+            for (let j = 0; j < bracketsConfig.length; j++) {
+                if (buff === bracketsConfig[j][0] && str[i] === bracketsConfig[j][1]) {
+                    isCorrect = true;
+                    break;
+                }
+                if (str[i] === bracketsConfig[j][0] || str[i] === bracketsConfig[j][1]) {
+                    isBracket = true;
+                }
+            }
+            if (!isCorrect) {
+                arr.push(buff);
+                if (isBracket) {
+                    arr.push(str[i]);
+                }
+            }
+        }
+    }
+    return arr.length === 0;
 }
 
 module.exports = {
